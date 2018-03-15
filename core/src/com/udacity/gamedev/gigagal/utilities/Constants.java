22c71ac7db75d262f3bcd5a8bab4f7d5362a7980
Michael Kemp <mkemp@accu-time.com>
2018-03-15 08:33:12 -0400

    Clean up constants

--- core/src/com/udacity/gamedev/gigagal/utilities/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/utilities/Constants.java
@@ -10,18 +10,25 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Constants {
 
+    // World/Camera
     public static final Color BACKGROUND_COLOR = Color.SKY;
-
-    /**
-     * Sprites in this game are drawn at their natural size.
-     * This size denotes the number of pixels that can fit to a screen.
-     * We're operating in landscape mode, using an extend viewport,
-     * so this is really the height of the world.
-     */
-    public static final float WORLD_SIZE = 128;
+    public static final float WORLD_SIZE = 160;
+    public static final float KILL_PLANE = -100;
+    public static final float GRAVITY = 10;
+    public static final float CHASE_CAM_MOVE_SPEED = WORLD_SIZE;
 
     public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
-    public static final String ENEMY_SPRITE     = "enemy";
+
+    // Giga Gal
+    public static final Vector2 GIGAGAL_EYE_POSITION = new Vector2(16, 24);
+    public static final float GIGAGAL_EYE_HEIGHT = 16f;
+    public static final float GIGAGAL_STANCE_WIDTH = 21f;
+    public static final float GIGAGAL_HEIGHT = 23f;
+    public static final float GIGAGAL_MOVEMENT_SPEED = 100;
+
+    public static final float GIGAGAL_JUMP_SPEED = 200;
+    public static final float GIGAGAL_JUMP_MAX_DURATION = 0.1f;
+
     public static final String STANDING_RIGHT   = "standing-right";
     public static final String STANDING_LEFT    = "standing-left";
     public static final String JUMPING_LEFT     = "jumping-left";
@@ -32,24 +39,13 @@ public class Constants {
     public static final String WALKING_LEFT_2   = "walk-2-left";
     public static final String WALKING_RIGHT_3  = "walk-3-right";
     public static final String WALKING_LEFT_3   = "walk-3-left";
-    public static final String PLATFORM         = "platform";
-
     public static final float WALK_LOOP_FRAME_DURATION = 0.25f;
+
+    // Platform
+    public static final String PLATFORM         = "platform";
     public static final int PLATFORM_EDGE = 8;
 
-    public static final Vector2 GIGAGAL_EYE_POSITION = new Vector2(16, 24);
-    public static final float GIGAGAL_EYE_HEIGHT = 16f;
-
-    public static final float GIGAGAL_STANCE_WIDTH = 21f;
-
-    public static final float GIGAGAL_MOVEMENT_SPEED = WORLD_SIZE / 2;
-
-    public static final float GIGAGAL_JUMP_SPEED = 1.5f * WORLD_SIZE;
-    public static final float GIGAGAL_JUMP_MAX_DURATION = 0.1f;
-
-    public static final float GRAVITY = WORLD_SIZE / 10;
-
-    public static final float KILL_PLANE = -100;
-
-    public static final float CHASE_CAM_MOVE_SPEED = 128;
+    // Enemy
+    public static final String ENEMY_SPRITE     = "enemy";
+    public static final Vector2 ENEMY_CENTER = new Vector2(14, 22);
 }
